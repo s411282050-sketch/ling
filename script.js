5af2ac4454e94d63678596a99706c98a6c76c0d9
s411282050-sketch <s411282050@gm.ntpu.edu.tw>
2026-04-27 15:22:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+// 花瓣生成
+for (let i = 0; i < 20; i++) {
+  const petal = document.createElement("div");
+  petal.className = "petal";
+  petal.style.left = Math.random() * 100 + "vw";
+  petal.style.animationDuration = 5 + Math.random() * 5 + "s";
+  document.body.appendChild(petal);
+}
